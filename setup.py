55405a12059673e2dde1514d79ab4bdb5f036f98
Agnibha <ab16ms076@iiserkol.ac.in>
2021-06-23 23:26:00 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='exospectra',
+    version='0.0.1',
+    packages=['exospectra'],
+    install_requires=[
+        'numpy',
+        'matplotlib',
+    ],
+)
